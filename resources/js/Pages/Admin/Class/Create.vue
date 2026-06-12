35c9637d7e79bc32377a36e834bd8b57ec7ac811
Your Name <you@example.com>
2026-06-12 21:39:04 +0700

    fix bug

--- resources/js/Pages/Admin/Class/Create.vue
+++ resources/js/Pages/Admin/Class/Create.vue
@@ -1,12 +1,11 @@
 <script setup>
 import { Head, useForm, usePage } from "@inertiajs/vue3";
-import SchoolLayout from "@/Layouts/SchoolLayout.vue"";
+import SchoolLayout from "@/Layouts/SchoolLayout.vue";
 import { ErrorMessage, Field, Form } from "vee-validate";
 import * as yup from "yup";
 import InputError from "@/Components/InputError.vue";
 import { useToast } from "vue-toastification";
 import { ref } from "vue";
-import SchoolLayout from "@/Layouts/SchoolLayout.vue";
 
 const toast = useToast();
 const form = useForm({
